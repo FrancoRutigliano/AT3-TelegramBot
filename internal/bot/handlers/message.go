5297fd3351ac478d0dd454b4f25e39b03e117c86
Franco Rutigliano <francorutigliano11@gmail.com>
2024-09-14 22:26:01 -0300

    remove offensive comments OK

--- internal/bot/handlers/message.go
+++ internal/bot/handlers/message.go
@@ -2,12 +2,24 @@ package handlers
 
 import (
 	"atomico3bot/internal/filters"
+	"log"
 
 	tgbotapi "gopkg.in/telegram-bot-api.v4"
 )
 
 func HandleMessage(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 	if filters.IsNegativeComment(message.Text) {
+
+		deleteMessage := tgbotapi.DeleteMessageConfig{
+			ChatID:    message.Chat.ID,
+			MessageID: message.MessageID,
+		}
+
+		_, err := bot.DeleteMessage(deleteMessage)
+		if err != nil {
+			log.Fatal("error al eliminar un comentario negativo")
+		}
+
 		msg := tgbotapi.NewMessage(message.Chat.ID, "Por favor evita malos comentarios")
 		bot.Send(msg)
 	}
